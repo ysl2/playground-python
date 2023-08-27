5662e835865ac55cbe8976c405194a37f37671f1
ysl2 <www.songli.yu@gmail.com>
2023-08-27 19:39:09 +0800

    Add clang-format into ctypes folder

--- ctypes/example.c
+++ ctypes/example.c
@@ -4,21 +4,21 @@
 // clang -shared -o example.so example.c
 // Windows:
 // clang -shared -o example.dll example.c
+#include "stdio.h"
 
 long long example(int x)
 {
-    long long y;
-    long long i;
-    int j;
+	long long y;
+	long long i;
+	int j;
 
-    for (j = 0; j < x; j++)
-    {
-        y = 0;
-        for (i = 0; i < x; i++)
-        {
-            y += i * i;
-        }
-    }
+	for (j = 0; j < x; j++) {
+		y = 0;
+		for (i = 0; i < x; i++) {
+			y += i * i;
+			printf("Hello");
+		}
+	}
 
-    return y;
+	return y;
 }
